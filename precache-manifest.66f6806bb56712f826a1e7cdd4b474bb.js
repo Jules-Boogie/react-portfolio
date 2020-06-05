e1990ed01e91034999ed7a4d2dadb9e3cdde08fa
Juliet George <georgejuliet11@gmail.com>
2020-06-04 20:57:58 -0700

    Updates

---
+++ precache-manifest.66f6806bb56712f826a1e7cdd4b474bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11d57a4dde1417d24cbb788d8c22171",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0a6518251d9847168fd9",
+    "url": "/react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0befae8c187dba65d1c6",
+    "url": "/react-portfolio/static/js/2.22cce7e2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.22cce7e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a6518251d9847168fd9",
+    "url": "/react-portfolio/static/js/main.52c4f927.chunk.js"
+  },
+  {
+    "revision": "be3cb1a3dfa81366eb8c",
+    "url": "/react-portfolio/static/js/runtime-main.5ca49836.js"
+  },
+  {
+    "revision": "49f8a5a747cb789ac2b1dc10e2f496ed",
+    "url": "/react-portfolio/static/media/IMG_1878.49f8a5a7.JPG"
+  },
+  {
+    "revision": "c881ca425a7715160373bdb6a5754542",
+    "url": "/react-portfolio/static/media/aboutpic.c881ca42.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);

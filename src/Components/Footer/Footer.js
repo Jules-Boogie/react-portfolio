ef213d4b0570c57b879ed543c6377b74e76fd7af
Juliet George <georgejuliet11@gmail.com>
2020-06-05 00:18:07 -0700

    working deployment

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Social from "../Social/Social"
 import  {Navbar} from 'react-bootstrap'
+import {Link} from "react-router-dom"
 
 
 function footer(){
@@ -19,20 +20,26 @@ return (
          
             
                     <ul className="nav nav-bar">
-                        <li className="nav-item">
-                            <a className="nav-link active" href="/"><strong className="navBorder">Home</strong></a>
+                    <li className="nav-item">
+                            {/* <a className="nav-link active" href="/"><strong className="navBorder">Home</strong></a> */}
+                            <Link  className="navBorder nav-link" to="/"> Home </Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" href="/About"><strong className="navBorder">About</strong></a>
+                            {/* <a className="nav-link active" href="/About"><strong className="navBorder">About</strong></a> */}
+                            <Link className="navBorder nav-link" to="/About"> About </Link>
                         </li>
                         <br/>
                             <li className="nav-item">
-                                <a className="nav-link" href="/Portfolio"><strong
-                                    className="navBorder">Portfolio</strong></a>
+                                {/* <a className="nav-link" href="/Portfolio"><strong
+                                    className="navBorder">Portfolio</strong></a> */}
+
+                                    <Link className="navBorder nav-link" to="/Portfolio"> Portfolio</Link>
+                                    
                             </li>
                             <br/>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/Contact"><strong className="navBorder">Contact</strong></a>
+                                    {/* <a className="nav-link" href="/Contact"><strong className="navBorder">Contact</strong></a> */}
+                                    <Link className="navBorder nav-link" to="/Contact"> Contact </Link>
                                 </li>
                                 <br/>
                     </ul>
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import {Link} from "react-router-dom"
 
 
@@ -9,19 +10,25 @@ function navBar() {
                 <nav className="navbar fixed-top justify-content-end">
                     <ul className="nav nav-bar">
                         <li className="nav-item">
-                            <a className="nav-link active" href="/"><strong className="navBorder">Home</strong></a>
+                            {/* <a className="nav-link active" href="/"><strong className="navBorder">Home</strong></a> */}
+                            <Link  className="navBorder nav-link" to="/"> Home </Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" href="/About"><strong className="navBorder">About</strong></a>
+                            {/* <a className="nav-link active" href="/About"><strong className="navBorder">About</strong></a> */}
+                            <Link className="navBorder nav-link" to="/About"> About </Link>
                         </li>
                         <br/>
                             <li className="nav-item">
-                                <a className="nav-link" href="/Portfolio"><strong
-                                    className="navBorder">Portfolio</strong></a>
+                                {/* <a className="nav-link" href="/Portfolio"><strong
+                                    className="navBorder">Portfolio</strong></a> */}
+
+                                    <Link className="navBorder nav-link" to="/Portfolio"> Portfolio</Link>
+                                    
                             </li>
                             <br/>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/Contact"><strong className="navBorder">Contact</strong></a>
+                                    {/* <a className="nav-link" href="/Contact"><strong className="navBorder">Contact</strong></a> */}
+                                    <Link className="navBorder nav-link" to="/Contact"> Contact </Link>
                                 </li>
                                 <br/>
                     </ul>
